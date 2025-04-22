4a808ff99f260e09a21b5daad90cac7deb55b62d
mithilesh143 <mithiguptaommm143@gmail.com>
2025-04-22 17:14:39 +0530

    backend update

---
+++ src/app/api/login/route.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-semibold mb-4">Login</h2>
+      {/* Add login form */}
+    </div>
+  );
+};
+
+export default Login;
