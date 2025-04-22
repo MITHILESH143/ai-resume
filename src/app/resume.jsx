8c132de242d5ae78f93624127fab2034b7e6a7c3
mithilesh143 <mithiguptaommm143@gmail.com>
2025-04-22 16:09:19 +0530

    added

--- src/app/resume.jsx
+++ src/app/resume.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-const ResumeBuilder = () => {
+export default function Resume() {
   return (
     <div className="p-6">
       <h2 className="text-xl font-semibold">Build Your Resume</h2>
@@ -8,5 +6,3 @@ const ResumeBuilder = () => {
     </div>
   );
 };
-
-export default ResumeBuilder;
