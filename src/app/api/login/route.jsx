6aac20fbaf79cdb7ae499fba828034299156bfa2
mithilesh143 <mithiguptaommm143@gmail.com>
2025-04-25 15:02:50 +0530

    bcrypt liabary are used to encrption password

--- src/app/api/login/route.jsx
+++ src/app/api/login/route.jsx
@@ -1,11 +1,15 @@
 import connectToDatabase from '@/dbConfig/dbConnection';
 import User from '@/models/userModel';
 import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
 
 connectToDatabase();
 
 export async function POST(request) {
   try {
+
+    
+
     const { email, password } = await request.json();
 
     if (!email || !password) {
@@ -17,8 +21,10 @@ export async function POST(request) {
     if (!user) {
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
+    const ismatch = await bcrypt.compare(password, user.password);
+    
 
-    if (user.password !== password) {
+    if (!ismatch) { 
       return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
     }
 
--- src/app/api/register/route.jsx
+++ src/app/api/register/route.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import connectToDatabase from '@/dbConfig/dbConnection';
 import User from '@/models/userModel';
 import { NextRequest, NextResponse } from 'next/server';
-
-
+import bcrypt from "bcrypt";
 
 // Connect to the database
 connectToDatabase();
@@ -11,7 +10,8 @@ connectToDatabase();
 export async function POST(request) {
   try {
     const { username, email, password } = await request.json();
-
+    const saltRounds = 10; // Number of salt rounds for bcrypt hashing
+    const hashPassword = await bcrypt.hash(password, saltRounds);
     if (!username || !email || !password) {
       return NextResponse.json({ message: 'Missing fields' }, { status: 400 });
     }
@@ -20,8 +20,8 @@ export async function POST(request) {
     if (existingUser) {
       return NextResponse.json({ message: 'User already exists' }, { status: 409 });
     }
-
-    const newUser = new User({ username, email, password });
+  
+    const newUser = new User({ username, email, password:hashPassword });
     await newUser.save();
 
     return NextResponse.json({ message: 'user registered successfully' }, { status: 200 });
